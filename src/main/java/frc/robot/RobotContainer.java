7196ae6ba6d193e28e6f6517cf8558376f49fbe5
John <John.samson.2021@smacatholic.org>
2020-01-29 19:00:33 -0600

    Added button binding example for Michael

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,11 +7,10 @@
 
 package frc.robot;
 
-import com.ctre.phoenix.ButtonMonitor;
-
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Button;
+import frc.robot.commands.ClearGyroAndEncoders;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.JoystickArcadeDrive;
 import frc.robot.subsystems.ColorWheelSubsystem;
@@ -21,7 +20,6 @@ import frc.robot.subsystems.PickUpSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
@@ -33,7 +31,7 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
 
   public static final double DEAD_ZONE = 0.1;
-	public static final int DRIVE_AXIS = 1;
+  public static final int DRIVE_AXIS = 1;
   public static final int CURVE_AXIS = 4;
   
   
@@ -50,40 +48,15 @@ public class RobotContainer {
   private final PickUpSubsystem m_PickUpSubsystem = new PickUpSubsystem();
 
   private final XboxController driver = new XboxController(0);
-
-  private final JoystickButton driverY = new JoystickButton(driver, 4);
-	private final JoystickButton driverB = new JoystickButton(driver, 2);
-	private final JoystickButton driverA = new JoystickButton(driver, 1);
-	private final JoystickButton driverX = new JoystickButton(driver, 3);
-	private final JoystickButton driverBumperLeft = new JoystickButton(driver, 5);
-	private final JoystickButton driverBumperRight = new JoystickButton(driver, 6);
-	private final JoystickButton driverBack = new JoystickButton(driver, 7);
-	private final JoystickButton driverStart = new JoystickButton(driver, 8);
-/* 	private final Trigger driverLeftThrottleButton = new ThrottleButton(driver, LEFT_FRONT_THROTTLE);
-  private final Trigger driverRightThrottleButton = new ThrottleButton(driver, RIGHT_FRONT_THROTTLE); 
-  Was here */
-	private final POVButton driverPOV0 = new POVButton(driver, 0);
-	private final POVButton driverPOV90 = new POVButton(driver, 90);
-	private final POVButton driverPOV180 = new POVButton(driver, 180);
-	private final POVButton driverPOV270 = new POVButton(driver, 270);
-
-
-	private final XboxController secondary = new XboxController(1);
-	private final JoystickButton secondaryY = new JoystickButton(secondary, 4);
-	private final JoystickButton secondaryB = new JoystickButton(secondary, 2);
-	private final JoystickButton secondaryA = new JoystickButton(secondary, 1);
-	private final JoystickButton secondaryX = new JoystickButton(secondary, 3);
-	private final JoystickButton secondaryBumperLeft = new JoystickButton(secondary, 5);
-	private final JoystickButton secondaryBumperRight = new JoystickButton(secondary, 6);
-	private final JoystickButton secondaryBack = new JoystickButton(secondary, 7);
-	private final JoystickButton secondaryStart = new JoystickButton(secondary, 8);
-
+// Yes Michael I took all this stuff away.
+  private final XboxController secondary = new XboxController(1);
+	
   /**
    * The container for the robot.  Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
-    // Configure the button bindings
-    configureButtonBindings();
+	// Configure the button bindings
+	configureButtonBindings();
 
   }
 
@@ -94,7 +67,11 @@ public class RobotContainer {
    * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    m_driveSubsystem.setDefaultCommand(m_driveCommand);
+	m_driveSubsystem.setDefaultCommand(m_driveCommand);
+	
+	// Below sets the button A on "driver" to the command "ClearGyroAndEncoders"
+	new JoystickButton(driver, Button.kA.value).whenPressed(new ClearGyroAndEncoders(m_driveSubsystem)); 
+
   }
 
 
